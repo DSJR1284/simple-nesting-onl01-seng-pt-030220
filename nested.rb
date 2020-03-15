94c4b37371bd6d11105883468f32c939b5df11ee
Doug Jones <dsjr1284@gmail.com>
2020-03-15 02:46:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require "pry"
 
 def hopper
 	programmer_hash = 
@@ -56,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     binding.pry 
      programmer_hash[:dennis_ritchie][:languages]
 end
 
